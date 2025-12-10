e2450ef797ef9f14fc23463f2accf35ff71095b3
mj <mj@mj.com>
2025-12-10 14:58:51 +0330

    chore(url-create-schema): add field_validator for check is ttl in future or not

--- app/schemas/url.py
+++ app/schemas/url.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import HttpUrl
+from pydantic import HttpUrl, field_validator
 from datetime import datetime
 
 from app.schemas.base_schema import ShortCodeField, CamelBaseModel
@@ -9,6 +9,17 @@ from app.schemas.base_schema import ShortCodeField, CamelBaseModel
 class URLCreate(CamelBaseModel):
     url: HttpUrl
     ttl: Optional[datetime]
+
+    @field_validator('ttl')
+    def check_ttl_is_future(cls, v: Optional[datetime]) -> Optional[datetime]:
+        if v is None:
+            return v
+
+        current_time = datetime.utcnow()
+        if v <= current_time:
+            raise ValueError('TTL must be in the future')
+
+        return v
 
 
 class URLResponse(CamelBaseModel):
